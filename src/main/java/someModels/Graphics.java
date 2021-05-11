44cbd7522d81d6bba3fad7c963cd837aca2fd71b
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-11 19:05:09 +0300

    lombok Graphics.java

--- src/main/java/someModels/Graphics.java
+++ src/main/java/someModels/Graphics.java
@@ -1,17 +1,12 @@
 package someModels;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class Graphics {
-    private String brand;
+    private String producerBrand;
     private String type;
-    private String GPU;
+    private String technologyBrand;
     private int speed;
     private int sizeMemory;
-
-    public Graphics(String brand, String type, String GPU, int speed, int sizeMemory) {
-        this.brand = brand;
-        this.type = type;
-        this.GPU = GPU;
-        this.speed = speed;
-        this.sizeMemory = sizeMemory;
-    }
 }

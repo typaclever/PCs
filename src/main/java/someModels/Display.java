303eac7eae45409fe24c4c020c992f109e3d72ab
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-11 12:48:52 +0300

    fill Display.java

--- src/main/java/someModels/Display.java
+++ src/main/java/someModels/Display.java
@@ -1,4 +1,15 @@
 package someModels;
 
 public class Display {
+    private float diagonal;
+    private String resolution;
+    private String matrix;
+    private int brightness;
+
+    public Display(float diagonal, String resolution, String matrix, int brightness) {
+        this.diagonal = diagonal;
+        this.resolution = resolution;
+        this.matrix = matrix;
+        this.brightness = brightness;
+    }
 }

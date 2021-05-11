75f8983bf91cde2bae90cdb6f1f1eb47ff47f7dc
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-11 11:38:48 +0300

    fill RAM.java

--- src/main/java/someModels/RAM.java
+++ src/main/java/someModels/RAM.java
@@ -1,5 +1,17 @@
 package someModels;
 
 public class RAM {
+    String brand;
+    String tireType;
+    int frequency;
+    int throughput;
+    int size;
 
+    public RAM(String brand, String tireType, int frequency, int throughput, int size) {
+        this.brand = brand;
+        this.tireType = tireType;
+        this.frequency = frequency;
+        this.throughput = throughput;
+        this.size = size;
+    }
 }

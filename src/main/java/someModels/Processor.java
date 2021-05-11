28486097b0eae2ccf6ee6d161f6c5c5ff2dc5a55
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-11 11:56:30 +0300

    fill Processor.java

--- src/main/java/someModels/Processor.java
+++ src/main/java/someModels/Processor.java
@@ -1,4 +1,22 @@
 package someModels;
 
 public class Processor {
+    String brand;
+    String series;
+    String model;
+    int coresCount;
+    float baseFrequency;
+    float maxFrequency;
+    float cache;
+
+    public Processor(String brand, String series, String model, int coresCount, float baseFrequency,
+                     float maxFrequency, float cache) {
+        this.brand = brand;
+        this.series = series;
+        this.model = model;
+        this.coresCount = coresCount;
+        this.baseFrequency = baseFrequency;
+        this.maxFrequency = maxFrequency;
+        this.cache = cache;
+    }
 }

ce33970d913d76f9af8f11ee16aae695ee35a128
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-11 19:06:50 +0300

    lombok KeyboardAndTouchpad.java

--- src/main/java/someModels/KeyboardAndTouchpad.java
+++ src/main/java/someModels/KeyboardAndTouchpad.java
@@ -1,13 +1,10 @@
 package someModels;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class KeyboardAndTouchpad {
     private boolean numPad;
     private boolean keyboardLight;
     private float touchPadDiagonal;
-
-    public KeyboardAndTouchpad(boolean numPad, boolean keyboardLight, float touchPadDiagonal) {
-        this.numPad = numPad;
-        this.keyboardLight = keyboardLight;
-        this.touchPadDiagonal = touchPadDiagonal;
-    }
 }

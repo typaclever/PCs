eef9e1dd4fd1beb35240707b79f2c3906cd98a02
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-11 13:11:54 +0300

    fill KeyboardAndTouchpad.java

--- src/main/java/someModels/KeyboardAndTouchpad.java
+++ src/main/java/someModels/KeyboardAndTouchpad.java
@@ -1,4 +1,13 @@
 package someModels;
 
 public class KeyboardAndTouchpad {
+    private boolean numPad;
+    private boolean keyboardLight;
+    private float touchPadDiagonal;
+
+    public KeyboardAndTouchpad(boolean numPad, boolean keyboardLight, float touchPadDiagonal) {
+        this.numPad = numPad;
+        this.keyboardLight = keyboardLight;
+        this.touchPadDiagonal = touchPadDiagonal;
+    }
 }

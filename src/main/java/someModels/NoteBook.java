44a986091a3ef50f0d536dc04bb5e23d84e147e5
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-11 19:09:00 +0300

    spaces NoteBook.java

--- src/main/java/someModels/NoteBook.java
+++ src/main/java/someModels/NoteBook.java
@@ -4,11 +4,13 @@ public class NoteBook extends Computer {
     private Display display;
     private KeyboardAndTouchpad keyboardAndTouchpad;
     private int batterySize;
-    public NoteBook(RAM ram, Memory memory, Processor processor, Graphics graphics, OperationSystem os,
-                    int price, Display display, int batterySize , KeyboardAndTouchpad keyboardAndTouchpad) {
-        super(ram, memory, processor, graphics, os, price);
+
+    public NoteBook(RandomAccesMemory randomAccesMemory, Memory memory, Processor processor,
+                    Graphics graphics, OperationSystem operationSystem, int price, Display display,
+                    KeyboardAndTouchpad keyboardAndTouchpad, int batterySize) {
+        super(randomAccesMemory, memory, processor, graphics, operationSystem, price);
         this.display = display;
-        this.batterySize = batterySize;
         this.keyboardAndTouchpad = keyboardAndTouchpad;
+        this.batterySize = batterySize;
     }
 }

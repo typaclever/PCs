15e04e0462daad9b51221a09a8959f05316d7be4
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-11 19:09:21 +0300

    lombok OperationSystem.java

--- src/main/java/someModels/OperationSystem.java
+++ src/main/java/someModels/OperationSystem.java
@@ -1,13 +1,10 @@
 package someModels;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class OperationSystem {
     private String brand;
     private String name;
     private String version;
-
-    public OperationSystem(String brand, String name, String version) {
-        this.brand = brand;
-        this.name = name;
-        this.version = version;
-    }
 }

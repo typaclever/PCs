8302ae1b05dc6ccc5ad521e2aa3024be7871a6af
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-11 19:03:12 +0300

    lombok Display.java

--- src/main/java/someModels/Display.java
+++ src/main/java/someModels/Display.java
@@ -1,15 +1,11 @@
 package someModels;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class Display {
     private float diagonal;
     private String resolution;
     private String matrix;
     private int brightness;
-
-    public Display(float diagonal, String resolution, String matrix, int brightness) {
-        this.diagonal = diagonal;
-        this.resolution = resolution;
-        this.matrix = matrix;
-        this.brightness = brightness;
-    }
 }

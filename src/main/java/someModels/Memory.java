273620b71c8d109ed5414d8f1f0809f72c80409d
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-11 11:48:31 +0300

    fill Memory.java

--- src/main/java/someModels/Memory.java
+++ src/main/java/someModels/Memory.java
@@ -1,4 +1,19 @@
 package someModels;
 
 public class Memory {
+    String brand;
+    String type;
+    String connectionInterface;
+    int readSpeed;
+    int writeSpeed;
+    int size;
+
+    public Memory(String brand, String type, String connectionInterface, int readSpeed, int writeSpeed, int size) {
+        this.brand = brand;
+        this.type = type;
+        this.connectionInterface = connectionInterface;
+        this.readSpeed = readSpeed;
+        this.writeSpeed = writeSpeed;
+        this.size = size;
+    }
 }

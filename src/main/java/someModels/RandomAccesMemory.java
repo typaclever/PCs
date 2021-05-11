fb2eb6f27d193946a417fbbf672ecc141b60ac97
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-11 19:10:50 +0300

    rename RAM

---
+++ src/main/java/someModels/RandomAccesMemory.java
@@ -0,0 +1,12 @@
+package someModels;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class RandomAccesMemory {
+    private String brand;
+    private String tireType;
+    private int frequency;
+    private int throughput;
+    private int size;
+}

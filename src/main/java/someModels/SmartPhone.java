4111cb0ef41423f7a55c292357dbb3ba110e4fd6
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-11 13:01:05 +0300

    fill SmartPhone.java

--- src/main/java/someModels/SmartPhone.java
+++ src/main/java/someModels/SmartPhone.java
@@ -1,8 +1,14 @@
 package someModels;
 
 public class SmartPhone extends Computer{
-
-    public SmartPhone(RAM ram, Memory memory, Processor processor, Graphics graphics, OperationSystem os, int price) {
+    private Display display;
+    private Connection connection;
+    private int batterySize;
+    public SmartPhone(RAM ram, Memory memory, Processor processor, Graphics graphics, OperationSystem os,
+                      int price, Display display, Connection connection, int batterySize) {
         super(ram, memory, processor, graphics, os, price);
+        this.display = display;
+        this.connection = connection;
+        this.batterySize = batterySize;
     }
 }

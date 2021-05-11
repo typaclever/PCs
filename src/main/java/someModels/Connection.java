50387dd1e6a2b6f997fd1e449f0a71470d072869
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-11 12:54:36 +0300

    fill Connection.java

--- src/main/java/someModels/Connection.java
+++ src/main/java/someModels/Connection.java
@@ -1,4 +1,11 @@
 package someModels;
 
 public class Connection {
+    private String [] bands;
+    private String [] connections;
+
+    public Connection(String[] bands, String[] connections) {
+        this.bands = bands;
+        this.connections = connections;
+    }
 }

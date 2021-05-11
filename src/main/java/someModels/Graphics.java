94568dc2c77d0eb2c007045a08254750a14e94cb
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-11 12:36:59 +0300

    fill Graphics.java

--- src/main/java/someModels/Graphics.java
+++ src/main/java/someModels/Graphics.java
@@ -1,4 +1,17 @@
 package someModels;
 
 public class Graphics {
+    private String brand;
+    private String type;
+    private String GPU;
+    private int speed;
+    private int sizeMemory;
+
+    public Graphics(String brand, String type, String GPU, int speed, int sizeMemory) {
+        this.brand = brand;
+        this.type = type;
+        this.GPU = GPU;
+        this.speed = speed;
+        this.sizeMemory = sizeMemory;
+    }
 }

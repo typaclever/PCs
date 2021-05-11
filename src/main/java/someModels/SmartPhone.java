5c9616665433c94455517b50fa69066890590747
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-11 12:49:12 +0300

    add SmartPhone.java

---
+++ src/main/java/someModels/SmartPhone.java
@@ -0,0 +1,8 @@
+package someModels;
+
+public class SmartPhone extends Computer{
+
+    public SmartPhone(RAM ram, Memory memory, Processor processor, Graphics graphics, OperationSystem os, int price) {
+        super(ram, memory, processor, graphics, os, price);
+    }
+}

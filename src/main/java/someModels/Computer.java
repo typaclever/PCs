0c49abf1182de219d7fec026906733beb5fbbb20
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-11 12:59:27 +0300

    fill Computer.java

--- src/main/java/someModels/Computer.java
+++ src/main/java/someModels/Computer.java
@@ -1,5 +1,20 @@
 package someModels;
 
 public class Computer {
+    private RAM ram;
+    private Memory memory;
+    private Processor processor;
+    private Graphics graphics;
+    private OperationSystem os;
+    private int price;
 
+    public Computer(RAM ram, Memory memory, Processor processor, Graphics graphics,
+                    OperationSystem os, int price) {
+        this.ram = ram;
+        this.memory = memory;
+        this.processor = processor;
+        this.graphics = graphics;
+        this.os = os;
+        this.price = price;
+    }
 }

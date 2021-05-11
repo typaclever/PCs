ca39f434237e4d2f7ddd1ddae13abd9fa069033b
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-11 19:10:11 +0300

    lombok Processor.java

--- src/main/java/someModels/Processor.java
+++ src/main/java/someModels/Processor.java
@@ -1,5 +1,8 @@
 package someModels;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class Processor {
     private String brand;
     private String series;
@@ -8,15 +11,4 @@ public class Processor {
     private float baseFrequency;
     private float maxFrequency;
     private float cache;
-
-    public Processor(String brand, String series, String model, int coresCount, float baseFrequency,
-                     float maxFrequency, float cache) {
-        this.brand = brand;
-        this.series = series;
-        this.model = model;
-        this.coresCount = coresCount;
-        this.baseFrequency = baseFrequency;
-        this.maxFrequency = maxFrequency;
-        this.cache = cache;
-    }
 }

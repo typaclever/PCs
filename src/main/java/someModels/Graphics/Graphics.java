ea6e6436050009c0b634daf9f5834c8b367527f2
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-13 18:03:09 +0300

    @Data + package for Graphics

---
+++ src/main/java/someModels/Graphics/Graphics.java
@@ -0,0 +1,21 @@
+package someModels.Graphics;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Graphics {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String producerBrand;
+    private String type;
+    private String technologyBrand;
+    private int speed;
+    private int sizeMemory;
+}

03ad2f3aace012ee5b0a778c7647e8dc7ef031e9
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-13 18:04:16 +0300

    @Data + package for Memory

---
+++ src/main/java/someModels/Memory/Memory.java
@@ -0,0 +1,22 @@
+package someModels.Memory;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Memory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String brand;
+    private String type;
+    private String connectionInterface;
+    private int readSpeed;
+    private int writeSpeed;
+    private int size;
+}

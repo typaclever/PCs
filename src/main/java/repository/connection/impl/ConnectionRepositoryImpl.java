3927128fbac7af70d487546a85b2625542c8e26b
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-14 21:54:10 +0300

    fill ConnectionRepositoryImpl

--- src/main/java/repository/connection/impl/ConnectionRepositoryImpl.java
+++ src/main/java/repository/connection/impl/ConnectionRepositoryImpl.java
@@ -1,4 +1,57 @@
 package repository.connection.impl;
 
-public class ConnectionRepositoryImpl {
+import model.connection.Connection;
+import org.hibernate.Session;
+import repository.connection.ConnectionRepository;
+import repository.sessionFactory.SessionFactoryAccess;
+
+import javax.persistence.Query;
+import java.util.List;
+
+public class ConnectionRepositoryImpl implements ConnectionRepository {
+    @Override
+    public Connection save(Connection connection) {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.save(connection);
+        session.getTransaction().commit();
+        session.close();
+        return connection;
+    }
+
+    @Override
+    public Connection update(Connection connection) {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.update(connection);
+        session.getTransaction().commit();
+        session.close();
+        return connection;
+    }
+
+    @Override
+    public Connection delete(Connection connection) {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.delete(connection);
+        session.getTransaction().commit();
+        session.close();
+        return connection;
+    }
+
+    @Override
+    public Connection findById(int id) {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        Connection connection = session.get(Connection.class, id);
+        session.close();
+        return connection;
+    }
+
+    @Override
+    public List getAllConnections() {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        List connections = session.createQuery("from Connection").list();
+        session.close();
+        return connections;
+    }
 }

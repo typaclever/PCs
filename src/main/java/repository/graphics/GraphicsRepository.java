4850b7b5170dce1075c7f86d9f0eb6c79c06b887
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-14 13:01:37 +0300

    added method to get all records

--- src/main/java/repository/graphics/GraphicsRepository.java
+++ src/main/java/repository/graphics/GraphicsRepository.java
@@ -2,9 +2,12 @@ package repository.graphics;
 
 import model.graphics.Graphics;
 
+import java.util.List;
+
 public interface GraphicsRepository {
     Graphics save(Graphics graphics);
     Graphics update(Graphics graphics);
     Graphics delete(Graphics graphics);
     Graphics findById(int id);
+    List<Graphics> getAllGraphics();
 }

a8eeeb039f7fce414f0a6a24ddf12688c770fd3d
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-14 11:34:41 +0300

    fix imports

--- src/main/java/someModels/Computer/Computer.java
+++ src/main/java/someModels/Computer/Computer.java
@@ -7,7 +7,12 @@ import someModels.OperationSystem.OperationSystem;
 import someModels.Processor.Processor;
 import someModels.RandomAccesMemory.RandomAccessMemory;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 @Entity
 @Data

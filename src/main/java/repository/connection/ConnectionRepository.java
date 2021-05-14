5ef10e53c6d17819fa4f80c8463a67ece62dddb5
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-14 13:01:28 +0300

    added method to get all records

--- src/main/java/repository/connection/ConnectionRepository.java
+++ src/main/java/repository/connection/ConnectionRepository.java
@@ -2,9 +2,12 @@ package repository.connection;
 
 import model.connection.Connection;
 
+import java.util.List;
+
 public interface ConnectionRepository {
     Connection save(Connection connection);
     Connection update(Connection connection);
     Connection delete(Connection connection);
     Connection findById(int id);
+    List<Connection> getAllConnections();
 }

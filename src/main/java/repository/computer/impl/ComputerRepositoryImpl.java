9ffe66a33096a2be4a34ec76f8c398ad6386775f
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-14 21:53:54 +0300

    fill ComputerRepositoryImpl

--- src/main/java/repository/computer/impl/ComputerRepositoryImpl.java
+++ src/main/java/repository/computer/impl/ComputerRepositoryImpl.java
@@ -1,4 +1,57 @@
 package repository.computer.impl;
 
-public class ComputerRepositoryImpl {
+
+import model.computer.Computer;
+import org.hibernate.Session;
+import repository.computer.ComputerRepository;
+import repository.sessionFactory.SessionFactoryAccess;
+
+import java.util.List;
+
+public class ComputerRepositoryImpl implements ComputerRepository {
+    @Override
+    public Computer save(Computer computer) {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.save(computer);
+        session.getTransaction().commit();
+        session.close();
+        return computer;
+    }
+
+    @Override
+    public Computer update(Computer computer) {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.update(computer);
+        session.getTransaction().commit();
+        session.close();
+        return computer;
+    }
+
+    @Override
+    public Computer delete(Computer computer) {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.delete(computer);
+        session.getTransaction().commit();
+        session.close();
+        return computer;
+    }
+
+    @Override
+    public List getAllComputers() {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        List computers = session.createQuery("from Computer").list();
+        session.close();
+        return computers;
+    }
+
+    @Override
+    public Computer findById(int id) {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        Computer computer = session.get(Computer.class, id);
+        session.close();
+        return computer;
+    }
 }

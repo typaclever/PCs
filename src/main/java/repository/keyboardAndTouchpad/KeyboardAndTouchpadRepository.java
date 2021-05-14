975111cd3cad751750c9e2cde06efab9e3d52af5
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-14 13:01:40 +0300

    added method to get all records

--- src/main/java/repository/keyboardAndTouchpad/KeyboardAndTouchpadRepository.java
+++ src/main/java/repository/keyboardAndTouchpad/KeyboardAndTouchpadRepository.java
@@ -2,9 +2,12 @@ package repository.keyboardAndTouchpad;
 
 import model.keyboardAndTouchpad.KeyboardAndTouchpad;
 
+import java.util.List;
+
 public interface KeyboardAndTouchpadRepository {
     KeyboardAndTouchpad save(KeyboardAndTouchpad keyboardAndTouchpad);
     KeyboardAndTouchpad update(KeyboardAndTouchpad keyboardAndTouchpad);
     KeyboardAndTouchpad delete(KeyboardAndTouchpad keyboardAndTouchpad);
     KeyboardAndTouchpad findById(int id);
+    List<KeyboardAndTouchpad> getAllKeyboardAndTouchpads();
 }

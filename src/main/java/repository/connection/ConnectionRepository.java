43c67a1c276b0c19f113632effa15b0be2070e0b
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-14 12:49:05 +0300

    fix return in ConnectionRepository

--- src/main/java/repository/connection/ConnectionRepository.java
+++ src/main/java/repository/connection/ConnectionRepository.java
@@ -3,8 +3,8 @@ package repository.connection;
 import model.connection.Connection;
 
 public interface ConnectionRepository {
-    void save(Connection connection);
-    void update(Connection connection);
-    void delete(Connection connection);
+    Connection save(Connection connection);
+    Connection update(Connection connection);
+    Connection delete(Connection connection);
     Connection findById(int id);
 }

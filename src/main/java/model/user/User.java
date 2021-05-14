080fb036374a433e3bb7cb5e566b9a38b9ccd545
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-14 21:10:01 +0300

    fix User

--- src/main/java/model/user/User.java
+++ src/main/java/model/user/User.java
@@ -3,17 +3,24 @@ package model.user;
 import lombok.Data;
 import model.computer.Computer;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.List;
 
 @Data
 @Entity
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    private String logIn;
-    private String password;
+    String logIn;
+    private byte[] salt;
+    private byte[] hashPassword;
     @OneToMany(cascade = CascadeType.PERSIST, mappedBy = "user", fetch = FetchType.LAZY)
     private List<Computer> computers;
 }

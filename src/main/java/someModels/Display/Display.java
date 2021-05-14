f0d17d6121f4a34640a446d3261b5f6054a21b90
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-14 11:35:44 +0300

    fix imports

--- src/main/java/someModels/Display/Display.java
+++ src/main/java/someModels/Display/Display.java
@@ -2,7 +2,11 @@ package someModels.Display;
 
 import lombok.Data;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 
 @Data
 @Entity

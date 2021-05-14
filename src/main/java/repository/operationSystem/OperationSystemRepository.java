c7416fb688234a876ee2fcda3c54c5a1477dfc44
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-14 13:01:49 +0300

    added method to get all records

--- src/main/java/repository/operationSystem/OperationSystemRepository.java
+++ src/main/java/repository/operationSystem/OperationSystemRepository.java
@@ -2,9 +2,12 @@ package repository.operationSystem;
 
 import model.operationSystem.OperationSystem;
 
+import java.util.List;
+
 public interface OperationSystemRepository {
     OperationSystem save(OperationSystem operationSystem);
     OperationSystem update(OperationSystem operationSystem);
     OperationSystem delete(OperationSystem operationSystem);
     OperationSystem findById(int id);
+    List<OperationSystem> getAllOperationSystems();
 }

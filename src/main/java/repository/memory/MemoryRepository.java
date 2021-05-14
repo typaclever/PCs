cea132b93beaceccc495ce34dfdd2b3cf6ae95fc
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-14 13:01:45 +0300

    added method to get all records

--- src/main/java/repository/memory/MemoryRepository.java
+++ src/main/java/repository/memory/MemoryRepository.java
@@ -2,9 +2,12 @@ package repository.memory;
 
 import model.memory.Memory;
 
+import java.util.List;
+
 public interface MemoryRepository {
     Memory save(Memory memory);
     Memory update(Memory memory);
     Memory delete(Memory memory);
     Memory findById(int id);
+    List<Memory> getAllMemories();
 }
--- src/main/java/repository/noteBook/NoteBookRepository.java
+++ src/main/java/repository/noteBook/NoteBookRepository.java
@@ -2,9 +2,12 @@ package repository.noteBook;
 
 import model.noteBook.NoteBook;
 
+import java.util.List;
+
 public interface NoteBookRepository {
     NoteBook save(NoteBook noteBook);
     NoteBook update(NoteBook noteBook);
     NoteBook delete(NoteBook noteBook);
     NoteBook findById(int id);
+    List<NoteBook> getAllNoteBooks();
 }

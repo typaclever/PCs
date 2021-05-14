dcc7731bb42f3790d03ec166cf1de9faeb62137d
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-14 12:49:49 +0300

    fix return in GraphicsRepository

--- src/main/java/repository/graphics/GraphicsRepository.java
+++ src/main/java/repository/graphics/GraphicsRepository.java
@@ -3,8 +3,8 @@ package repository.graphics;
 import model.graphics.Graphics;
 
 public interface GraphicsRepository {
-    void save(Graphics graphics);
-    void update(Graphics graphics);
-    void delete(Graphics graphics);
+    Graphics save(Graphics graphics);
+    Graphics update(Graphics graphics);
+    Graphics delete(Graphics graphics);
     Graphics findById(int id);
 }

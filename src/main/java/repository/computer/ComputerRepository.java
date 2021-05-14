3fd494b583b14a5dfb4c1719216fba434b902f06
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-14 12:48:57 +0300

    fix return in ComputerRepository

--- src/main/java/repository/computer/ComputerRepository.java
+++ src/main/java/repository/computer/ComputerRepository.java
@@ -5,6 +5,6 @@ import model.computer.Computer;
 public interface ComputerRepository {
     Computer save(Computer computer);
     Computer update(Computer computer);
-    void delete(Computer computer);
+    Computer delete(Computer computer);
     Computer findById(int id);
 }

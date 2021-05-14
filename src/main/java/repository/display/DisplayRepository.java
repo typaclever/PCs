f7bd7ada499c31163a607f5d6c70c65154892845
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-14 12:49:31 +0300

    fix return in DisplayRepository

--- src/main/java/repository/display/DisplayRepository.java
+++ src/main/java/repository/display/DisplayRepository.java
@@ -3,8 +3,8 @@ package repository.display;
 import model.display.Display;
 
 public interface DisplayRepository {
-    void save(Display display);
-    void update(Display display);
-    void delete(Display display);
+    Display save(Display display);
+    Display update(Display display);
+    Display delete(Display display);
     Display findById(int id);
 }

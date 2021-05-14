455061606d3bf54d3d49571785c7d78d3e463d8a
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-14 13:01:52 +0300

    added method to get all records

--- src/main/java/repository/processor/ProcessorRepository.java
+++ src/main/java/repository/processor/ProcessorRepository.java
@@ -2,9 +2,12 @@ package repository.processor;
 
 import model.processor.Processor;
 
+import java.util.List;
+
 public interface ProcessorRepository {
     Processor save(Processor processor);
     Processor update(Processor processor);
     Processor delete(Processor processor);
     Processor findById(int id);
+    List<Processor> getAllProcessor();
 }

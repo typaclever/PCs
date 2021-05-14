ea2bd874df9a768212361d2152dbcaedb244deed
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-14 21:54:20 +0300

    fill DisplayRepositoryImpl

--- src/main/java/repository/display/impl/DisplayRepositoryImpl.java
+++ src/main/java/repository/display/impl/DisplayRepositoryImpl.java
@@ -1,4 +1,56 @@
 package repository.display.impl;
 
-public class DisplayRepositoryImpl {
+import model.display.Display;
+import org.hibernate.Session;
+import repository.display.DisplayRepository;
+import repository.sessionFactory.SessionFactoryAccess;
+
+import java.util.List;
+
+public class DisplayRepositoryImpl implements DisplayRepository{
+    @Override
+    public Display save(Display display) {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.save(display);
+        session.getTransaction().commit();
+        session.close();
+        return display;
+    }
+
+    @Override
+    public Display update(Display display) {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.update(display);
+        session.getTransaction().commit();
+        session.close();
+        return display;
+    }
+
+    @Override
+    public Display delete(Display display) {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.delete(display);
+        session.getTransaction().commit();
+        session.close();
+        return display;
+    }
+
+    @Override
+    public Display findById(int id) {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        Display display = session.get(Display.class, id);
+        session.close();
+        return display;
+    }
+
+    @Override
+    public List getAllDisplays() {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        List displays = session.createQuery("from Display").list();
+        session.close();
+        return displays;
+    }
 }

5d62b5d8a34ee972cafabc027e17693de0fde2fd
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-14 11:36:02 +0300

    fix imports

--- src/main/java/someModels/RandomAccesMemory/RandomAccessMemory.java
+++ src/main/java/someModels/RandomAccesMemory/RandomAccessMemory.java
@@ -2,7 +2,10 @@ package someModels.RandomAccesMemory;
 
 import lombok.Data;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @Entity

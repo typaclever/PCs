761018b012b8fc4f5981b7830122738d93d2f0bc
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-14 21:55:25 +0300

    fill KeyboardAndTouchpadRepositoryImpl

--- src/main/java/repository/keyboardAndTouchpad/impl/KeyboardAndTouchpadRepositoryImpl.java
+++ src/main/java/repository/keyboardAndTouchpad/impl/KeyboardAndTouchpadRepositoryImpl.java
@@ -1,4 +1,57 @@
 package repository.keyboardAndTouchpad.impl;
 
-public class KeyboardAndTouchpadRepositoryImpl {
+import model.keyboardAndTouchpad.KeyboardAndTouchpad;
+import org.hibernate.Session;
+import repository.keyboardAndTouchpad.KeyboardAndTouchpadRepository;
+import repository.sessionFactory.SessionFactoryAccess;
+
+import java.util.List;
+
+public class KeyboardAndTouchpadRepositoryImpl implements KeyboardAndTouchpadRepository {
+    @Override
+    public KeyboardAndTouchpad save(KeyboardAndTouchpad keyboardAndTouchpad) {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.save(keyboardAndTouchpad);
+        session.getTransaction().commit();
+        session.close();
+        return keyboardAndTouchpad;
+    }
+
+    @Override
+    public KeyboardAndTouchpad update(KeyboardAndTouchpad keyboardAndTouchpad) {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.update(keyboardAndTouchpad);
+        session.getTransaction().commit();
+        session.close();
+        return keyboardAndTouchpad;
+    }
+
+    @Override
+    public KeyboardAndTouchpad delete(KeyboardAndTouchpad keyboardAndTouchpad) {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.delete(keyboardAndTouchpad);
+        session.getTransaction().commit();
+        session.close();
+        return keyboardAndTouchpad;
+    }
+
+    @Override
+    public KeyboardAndTouchpad findById(int id) {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        KeyboardAndTouchpad keyboardAndTouchpad = session.get(KeyboardAndTouchpad.class, id);
+        session.close();
+        return keyboardAndTouchpad;
+    }
+
+    @Override
+    public List getAllKeyboardAndTouchpads() {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        List keyboardAndTouchpads = session.
+                createQuery("from KeyboardAndTouchpad").list();
+        session.close();
+        return keyboardAndTouchpads;
+    }
 }

3d918e6518ebcaea84ceb551c024354057769498
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-14 13:01:59 +0300

    added method to get all records

--- src/main/java/repository/smartPhone/SmartPhoneRepository.java
+++ src/main/java/repository/smartPhone/SmartPhoneRepository.java
@@ -2,9 +2,12 @@ package repository.smartPhone;
 
 import model.smartPhone.SmartPhone;
 
+import java.util.List;
+
 public interface SmartPhoneRepository {
     SmartPhone save(SmartPhone smartPhone);
     SmartPhone update(SmartPhone smartPhone);
     SmartPhone delete(SmartPhone smartPhone);
     SmartPhone findById(int id);
+    List<SmartPhone> getAllSmartphones();
 }

4117b9fcb25cffc1c7203dc89af8918a5889277c
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-14 12:49:59 +0300

    fix return in KeyboardAndTouchpadRepository

--- src/main/java/repository/keyboardAndTouchpad/KeyboardAndTouchpadRepository.java
+++ src/main/java/repository/keyboardAndTouchpad/KeyboardAndTouchpadRepository.java
@@ -3,8 +3,8 @@ package repository.keyboardAndTouchpad;
 import model.keyboardAndTouchpad.KeyboardAndTouchpad;
 
 public interface KeyboardAndTouchpadRepository {
-    void save(KeyboardAndTouchpad keyboardAndTouchpad);
-    void update(KeyboardAndTouchpad keyboardAndTouchpad);
-    void delete(KeyboardAndTouchpad keyboardAndTouchpad);
+    KeyboardAndTouchpad save(KeyboardAndTouchpad keyboardAndTouchpad);
+    KeyboardAndTouchpad update(KeyboardAndTouchpad keyboardAndTouchpad);
+    KeyboardAndTouchpad delete(KeyboardAndTouchpad keyboardAndTouchpad);
     KeyboardAndTouchpad findById(int id);
 }

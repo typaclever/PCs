60a7773e034bd4b66fd4b12aac7f77293d9021fb
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-15 13:47:43 +0300

    add UserRepositoryImpl

---
+++ src/main/java/repository/user/impl/UserRepositoryImpl.java
@@ -0,0 +1,56 @@
+package repository.user.impl;
+
+import model.user.User;
+import org.hibernate.Session;
+import repository.sessionFactory.SessionFactoryAccess;
+import repository.user.UserRepository;
+
+import java.util.List;
+
+public class UserRepositoryImpl implements UserRepository{
+    @Override
+    public User save(User user) {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.save(user);
+        session.getTransaction().commit();
+        session.close();
+        return user;
+    }
+
+    @Override
+    public User update(User user) {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.update(user);
+        session.getTransaction().commit();
+        session.close();
+        return user;
+    }
+
+    @Override
+    public User delete(User user) {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.delete(user);
+        session.getTransaction().commit();
+        session.close();
+        return user;
+    }
+
+    @Override
+    public User findById(int id) {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        User user = session.get(User.class, id);
+        session.close();
+        return user;
+    }
+
+    @Override
+    public List<User> findAllUsers() {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        List<User> users = (List<User>) session.createQuery("from User").list();
+        session.close();
+        return users;
+    }
+}

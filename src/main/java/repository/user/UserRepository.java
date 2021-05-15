4ab6f77c60b7339597ccf3e00b297068e4a75d5f
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-15 13:47:29 +0300

    add UserRepository

---
+++ src/main/java/repository/user/UserRepository.java
@@ -0,0 +1,13 @@
+package repository.user;
+
+import model.user.User;
+
+import java.util.List;
+
+public interface UserRepository {
+    User save(User user);
+    User update(User user);
+    User delete(User user);
+    User findById(int id);
+    List<User> findAllUsers();
+}

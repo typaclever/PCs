d24d62b7907e8c410e4b05a26ae6ef5ea8568a46
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-15 13:50:43 +0300

    fill SmartPhoneRepositoryImpl

--- src/main/java/repository/smartPhone/impl/SmartPhoneRepositoryImpl.java
+++ src/main/java/repository/smartPhone/impl/SmartPhoneRepositoryImpl.java
@@ -1,4 +1,56 @@
 package repository.smartPhone.impl;
 
-public class SmartPhoneRepositoryImpl {
+import model.smartPhone.SmartPhone;
+import org.hibernate.Session;
+import repository.sessionFactory.SessionFactoryAccess;
+import repository.smartPhone.SmartPhoneRepository;
+
+import java.util.List;
+
+public class SmartPhoneRepositoryImpl implements SmartPhoneRepository{
+    @Override
+    public SmartPhone save(SmartPhone smartPhone) {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.save(smartPhone);
+        session.getTransaction().commit();
+        session.close();
+        return smartPhone;
+    }
+
+    @Override
+    public SmartPhone update(SmartPhone smartPhone) {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.update(smartPhone);
+        session.getTransaction().commit();
+        session.close();
+        return smartPhone;
+    }
+
+    @Override
+    public SmartPhone delete(SmartPhone smartPhone) {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.delete(smartPhone);
+        session.getTransaction().commit();
+        session.close();
+        return smartPhone;
+    }
+
+    @Override
+    public SmartPhone findById(int id) {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        SmartPhone smartPhone = session.get(SmartPhone.class, id);
+        session.close();
+        return smartPhone;
+    }
+
+    @Override
+    public List<SmartPhone> getAllSmartphones() {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        List<SmartPhone> smartPhones = (List<SmartPhone>) session.createQuery("from SmartPhone").list();
+        session.close();
+        return smartPhones;
+    }
 }

ba5eae8d2f1adef5822b753b35d4285d8a274846
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-15 13:49:47 +0300

    fill NoteBookRepositoryImpl

--- src/main/java/repository/noteBook/impl/NoteBookRepositoryImpl.java
+++ src/main/java/repository/noteBook/impl/NoteBookRepositoryImpl.java
@@ -1,4 +1,56 @@
 package repository.noteBook.impl;
 
-public class NoteBookRepositoryImpl {
+import model.noteBook.NoteBook;
+import org.hibernate.Session;
+import repository.noteBook.NoteBookRepository;
+import repository.sessionFactory.SessionFactoryAccess;
+
+import java.util.List;
+
+public class NoteBookRepositoryImpl implements NoteBookRepository {
+    @Override
+    public NoteBook save(NoteBook noteBook) {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.save(noteBook);
+        session.getTransaction().commit();
+        session.close();
+        return noteBook;
+    }
+
+    @Override
+    public NoteBook update(NoteBook noteBook) {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.update(noteBook);
+        session.getTransaction().commit();
+        session.close();
+        return noteBook;
+    }
+
+    @Override
+    public NoteBook delete(NoteBook noteBook) {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.delete(noteBook);
+        session.getTransaction().commit();
+        session.close();
+        return noteBook;
+    }
+
+    @Override
+    public NoteBook findById(int id) {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        NoteBook noteBook = session.get(NoteBook.class, id);
+        session.close();
+        return noteBook;
+    }
+
+    @Override
+    public List<NoteBook> getAllNoteBooks() {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        List<NoteBook> noteBooks = (List<NoteBook>) session.createQuery("from NoteBook").list();
+        session.close();
+        return null;
+    }
 }

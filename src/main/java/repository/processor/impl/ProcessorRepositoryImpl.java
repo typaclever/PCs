7d3a9441dc5bbf496df5bf235b8d65253e1c1e3d
typaclever <bogdan.cheestyakov@gmail.com>
2021-05-15 13:50:17 +0300

    fill ProcessorRepositoryImpl

--- src/main/java/repository/processor/impl/ProcessorRepositoryImpl.java
+++ src/main/java/repository/processor/impl/ProcessorRepositoryImpl.java
@@ -1,4 +1,56 @@
 package repository.processor.impl;
 
-public class ProcessorRepositoryImpl {
+import model.processor.Processor;
+import org.hibernate.Session;
+import repository.processor.ProcessorRepository;
+import repository.sessionFactory.SessionFactoryAccess;
+
+import java.util.List;
+
+public class ProcessorRepositoryImpl implements ProcessorRepository {
+    @Override
+    public Processor save(Processor processor) {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.save(processor);
+        session.getTransaction().commit();
+        session.close();
+        return processor;
+    }
+
+    @Override
+    public Processor update(Processor processor) {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.update(processor);
+        session.getTransaction().commit();
+        session.close();
+        return processor;
+    }
+
+    @Override
+    public Processor delete(Processor processor) {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.delete(processor);
+        session.getTransaction().commit();
+        session.close();
+        return processor;
+    }
+
+    @Override
+    public Processor findById(int id) {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        Processor processor = session.get(Processor.class, id);
+        session.close();
+        return processor;
+    }
+
+    @Override
+    public List<Processor> getAllProcessor() {
+        Session session = SessionFactoryAccess.getSessionFactory().openSession();
+        List<Processor> processors = (List<Processor>) session.createQuery("from Processor").list();
+        session.close();
+        return processors;
+    }
 }
